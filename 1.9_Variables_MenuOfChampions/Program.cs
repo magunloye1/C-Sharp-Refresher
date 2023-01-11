cd562456605c8cd21f3b9ec8d090e9276803c307
Marv Agunloye <marvelousagunloye@yahoo.com>
2023-01-10 17:11:04 -0700

    Demo: Collecting user input review

--- 1.9_Variables_MenuOfChampions/Program.cs
+++ 1.9_Variables_MenuOfChampions/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿
+//
+//Type your code into a Visual Studio project named MenuOfChampions.
+//Write a program that displays a menu for a restaurant.
+//The menu should include at least three different foods, each with its own price.
+//Each value (food name and price) should be stored in its own variable, using the appropriate type.
+//Display the restaurant name and menu, followed by a list of dishes and their prices, similar to the output shown below.
+//Feel free to use some (ASCII art)[https://asciiart.website//index.php?art=art%20and%20design/borders] to decorate your menu!
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +20,14 @@ namespace _1._9_Variables_MenuOfChampions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("WELCOME TO RESTAURANT NIGHT VALE!");
+            Console.WriteLine("Today's Menu Is...\n");
+
+            // $ 500.00 *** Slice of Big Rico Pizza
+            // $ 2.00 * ****Invisible Strawberry Pie
+            // $ 1.50 * ****Denver Omelet
+            Console.WriteLine("");
+
         }
     }
 }
---
+++ Demo1.1_CollectingUserInput/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1._1_CollectingUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int output; // storage for converted string
+
+            // infinite loop
+            while (true)
+            {
+                Console.Write("Enter a number between 1 and 10: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out output))
+                {
+                    // it was an int, now is it between 1 and 10?
+                    if (output >= 1 && output <= 10)
+                    {
+                        break; // good input
+                    }
+                    else
+                    {
+                        Console.WriteLine("That number was not between 1 and 10!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("That was not a number!");
+                }
+            }
+
+        }
+    }
+}
